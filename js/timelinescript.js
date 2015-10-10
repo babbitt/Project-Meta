1f66900f6f673621f9582b30f4811e70ca3d6e35
Joseph Babbitt <jrebbabbitt@gmail.com>
2015-10-10 15:49:11 -0400

    change everything again

--- js/timelinescript.js
+++ js/timelinescript.js
@@ -8,8 +8,8 @@ var ref = new Firebase('https://themetaproject.firebaseio.com');
 
 ref.on("value", function(snapshot) {
 	var wholeval = snapshot.val();
-	for (var projectname in wholeval) {
-		console.log(projectname);
+	  var url= window.location.href;
+      var projectname =url.slice(url.indexOf('id=')+3);
 	
   var name = snapshot.child(projectname + '/name').val();
   var events = snapshot.child(projectname +'/Events').val();
@@ -18,23 +18,56 @@ ref.on("value", function(snapshot) {
    	var Stamp = obj.Stamp;
    	var content = obj.content
    	var date = obj.date;
+   	var notes = obj.notes
+   	if (notes == undefined) {
+		notes = 'none';
+	}
    	console.log(Stamp, content, date);
    	var timelinenode = "<li class='tl-node'>\
-			<div class='tl-stamp'>"+ date +" </div>\
-			<div class='tl-content'>"+ Stamp +"</div>\
-			<a class='tl-link' href='#''>View notes</a>\
+			<div class='tl-stamp'>"+ Stamp +" </div>\
+			<div class='tl-content'>"+ content +"</div>\
+			<div id='" + name + "' class='tl-stamp notes'>"+ notes +" </div>\
 		</li>"
 	$('#timeline').append(timelinenode);
-
    	
    }
-}
-  //for(var eventname in events) {
-  	//console.log(eventname);
-
-  //}
- // console.log(events.Event1);
   document.getElementById("title").innerHTML = "[" + name + "]";
+  document.getElementById("prjtitle").innerHTML = "[" + name + "] Timeline";
+
+
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+  alert('Something firebase realted Broke, start praying to the demo gods');
+});
+
+ref.on("child_added", function(snapshot) {
+	document.getElementById("timeline").innerHTML = "";
+	var wholeval = snapshot.val();
+	  var url= window.location.href;
+      var projectname =url.slice(url.indexOf('id=')+3);
+	
+  var name = snapshot.child(projectname + '/name').val();
+  var events = snapshot.child(projectname +'/Events').val();
+   for (var key in events) {
+   	var obj = events[key];
+   	var Stamp = obj.Stamp;
+   	var content = obj.content
+   	var date = obj.date;
+   	var notes = obj.notes
+   	if (notes == undefined) {
+		notes = 'none';
+	}
+   	console.log(Stamp, content, date);
+   	var timelinenode = "<li class='tl-node'>\
+			<div class='tl-stamp'>"+ Stamp +" </div>\
+			<div class='tl-content'>"+ content +"</div>\
+			<div id='" + name + "' class='tl-stamp notes'>"+ notes +" </div>\
+		</li>"
+	$('#timeline').append(timelinenode);
+   	
+   }
+  document.getElementById("title").innerHTML = "[" + name + "]";
+  document.getElementById("prjtitle").innerHTML = "[" + name + "] Timeline";
 
 
 }, function (errorObject) {
